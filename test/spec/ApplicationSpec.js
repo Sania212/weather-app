a1adcbb4f982cbda8c9160ff4138cd026980893e
sania <212sania@gmail.com>
2016-04-05 15:10:15 -0400

    Update ApplicationSpec.js

--- test/spec/ApplicationSpec.js
+++ test/spec/ApplicationSpec.js
@@ -10,8 +10,10 @@ describe("Weather Application", function() {
 
     // verify
     expect(Math.round(result)).toEqual(50);
-  });
-  
+   
+    });
+
+
 
   it("successfully convert celsius into fahrenheit when celsius is zero", function() {
     // setup
@@ -22,8 +24,10 @@ describe("Weather Application", function() {
 
     // verify
     expect(Math.round(result)).toEqual(32);
-  });
-  
+   
+    });
+
+
 
   it("returns the proper day of the week abbreviation of Wednesday", function() {
     // setup
@@ -34,9 +38,11 @@ describe("Weather Application", function() {
     var result = getDay(knownDay);
 
     // verify
-    expect(result).toBe('Wed')
-  });
-  
+    expect(result).toEqual('Wed')
+   
+    });
+    
+    
 
   it("does not return the wrong day of the week abbreviation of Saturday", function() {
     // setup
@@ -48,7 +54,8 @@ describe("Weather Application", function() {
 
     // verify
     expect(result).not.toBe('Sat')
-  });
+    
+     });
   
   
   
@@ -62,20 +69,10 @@ describe("Weather Application", function() {
 
     // verify
     expect(result).not.toBe('Sun')
-  });
+  
+    });
   
 
-  it("successfully convert Fahrenheit into celsuis", function() {
-    // setup
-    var celsiusValue = 20;
-
-    // action
-    var result = convertToFahrenheit(celsiusValue);
-
-    // verify
-   expect(Math.round(convertToFahrenheit(25))).toBe(77);
-  });
-  
   
   it("retrieves full URL using country name without callback", function() {
     // setup
@@ -87,8 +84,11 @@ describe("Weather Application", function() {
 
     // verify
     expect(result).toContain('us');
+    
    expect(result).toContain('&APPID=0045c13635745b634c15955d56547ef3');
+   
     });  
+  
   
   
   it("retrieves correct minimum temperature of the current location", function() {
@@ -101,18 +101,22 @@ describe("Weather Application", function() {
 
     // verify
     expect(result).toContain('q=0');
-
+  
     expect(result).toContain('&units=metric');
 
 	});
 	
-
- it("should successfully retrieve the data of 5 days weather forecast of the current location ", function() { 
-  // setup
+	
+	
+  it("should successfully retrieve the data of 5 days weather forecast of the current location ", function() { 
+	
+	// setup
    var   forecastWeatherData='http://api.openweathermap.org/data/2.5/weather?';
- // action
+ 
+   // action
    var result= getFinalURL('http://api.openweathermap.org/data/2.5/weather?');
- // verify
+    
+    // verify
     expect(result).toContain(forecastWeatherData);
     });
 	
@@ -121,13 +125,20 @@ it("retrieves correct maximun temperature of the current location", function() {
     // setup
     var baseURL = 'http://api.openweathermap.org/data/2.5/weather?';
     var maxTemp = '10';
- // action
+
+
+    // action
     var result = getFinalURL(baseURL, maxTemp);
-  // verify
+
+    // verify
     expect(result).toContain('q=10');
     expect(result).toContain('&units=metric');
+
 	});
 	
+
+
+
 
   it("gets a proper full URL using city name without callback", function() {
     // setup
@@ -145,23 +156,89 @@ it("retrieves correct maximun temperature of the current location", function() {
     expect(result).toBe('http://api.openweathermap.org/data/2.5/weather?q=Denver,us&units=metric&APPID=0045c13635745b634c15955d56547ef3')
          
   });  
-    
+  });      
 
+
+
+ 
+ describe("Css TESTS", function() { 
+it("should have the correct font", function() {
+//setup
+      var fontFace = '"Arial", sans-serif'; 
+      
+ //action
+      var result =       
+      $("#sandbox").css("font-family", fontFace);
+      
+ //verify
+   
+      expect(result).toHaveCss();
+  
+         
   });  
- 	it("Sections contains the css class", function() {
- 	  // setup
-	var result;
-	beforeEach(function() {
-		elem = $ ('<div id="section"><a id="units">C</a></div>');
-  // verify
-		expect(elem).toBe("#section");
-		expect(elem).toContain('units');
+  });      
+
+
+
+
+//Fixture setup for (Tests for Unit button visibility on app)
+
+function setUpHTMLFixture() {
+        jasmine.getFixtures().set('   <div class="weather-details hidden">       \
+                                    <section class="weather-head">\
+                                    <h3></h3>                                           \
+                                    <a id="units">C</a>           \
+                                    </section>       \
+                                    <div class="weather-now"></div>              \
+                                    <br>                             \
+                                    <div class="weather-forecast"> </div> </div>   \
+                                ');
+}
+
+
+describe("Tests for Unit button visibility on app", function() {
+     beforeEach(function() {
+ 		setUpHTMLFixture();
+    });
+
+    it("should result in Units being visible", function() {
+    	// no action
+    	
+    	// verify
+        expect($("#units")).toBeVisible();
+    });
+    
+   it("should result units to have correct abbreviation ", function() {
+   	// no action
+   		
+   	// verify
+    	expect($( "#units" )).toHaveText("C");
+    });   
+});    
+        
+  
+  
+        
+describe("A Click event has been called ", function() {
+  var click;
+
+  beforeEach(function() {
+    click = jasmine.createSpy('.click');
+  });
+
+  it("tracks that the spy was called", function() {
+  	// action
+  	    click(".click");
 		
-	});
+	// verify
+    	expect(click).toHaveBeenCalled();
+  });
+
+
 });
 
 
-    
 
-  
-  
+
+
+
